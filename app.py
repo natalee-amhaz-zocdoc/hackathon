4b639f635b5b20be493ae79c9489f20820efd73d
Lexi Jeong <lexi.jeong@zocdoc.com>
2023-07-27 19:14:34 -0400

    implementing openai with infrastructure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS 
 import openai
+from openai_logic.chatgpt import get_answer
 
 app = Flask(__name__)
 CORS(app,  resources={r"/api/*": {"origins": "http://localhost:3000"}})
@@ -16,7 +17,7 @@ def run_gpt3():
     data = request.json
     prompt = data.get('prompt', '')
     #response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=150)
-    return jsonify(prompt)
+    return jsonify(get_answer(prompt))
 
 def add_question_to_report(text):
     try:
--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,6 @@ const App = () => {
     console.log("wubmitting");
     try {
       const response = await axios.post('http://localhost:5000/api/gpt3', { prompt: inputText });
-      console.log("test");
-      console.log(response);
       setResponseText(response.data);
     } catch (error) {
       console.error('Error:', error);
